be55451285602ad8095b25e20cdc16c54bf9948d
fireztron <84250829+fireztron@users.noreply.github.com>
2021-05-30 16:21:07 -0400

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,140 @@
+--[[
+    Attacking manually after running this will not affect how fast you kill.
+    Idk what the difference between parrying and blocking but all i know is parrying is more op
+
+    Made by fireztron @ v3rm
+]]
+
+--// For Non-Synapse Users (credits to egg salad)
+if not pcall(function() return syn.protect_gui end) then
+    syn = {}
+    syn.protect_gui = function(egg)
+        egg.Parent = game.CoreGui
+    end
+end
+
+--// lib stuff uwuware i think
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/fireztron/uwuware-ui-library/main/ui.lua', true))()
+local window = lib:CreateWindow('Trade Tower GUI')
+window:AddLabel({text = "fireztron @ v3rmillion"})
+
+--// auto attack UI
+window:AddToggle({text = 'Auto attack', state = autoattack, callback = function(v) 
+    autoattack = v; 
+end})
+
+--// godmode UI
+window:AddToggle({text = 'Godmode', state = godmode, callback = function(v) 
+    godmode = v; 
+end})
+
+--// walkspeed UI
+window:AddToggle({text = 'Walkspeed Changer', state = ws, callback = function(v) 
+    ws = v; 
+end})
+
+local walkspeedToChangeTo = 22
+--// walkspeed options ui
+local walkspeedoption = window:AddFolder("walkspeed option")
+walkspeedoption:AddSlider({text = 'walkspeed', value = 22, min = 12, max = 22, float = 1, callback = function(v) walkspeedToChangeTo = v end})
+
+--// Init library
+lib:Init()
+
+local mt = getrawmetatable(game) 
+local oldnamecall = mt.__namecall
+setreadonly(mt, false)
+
+--// Always parry
+mt.__namecall = newcclosure(function(self, ...)
+   local method = getnamecallmethod()
+   local Args = {...}
+   if not checkcaller() and method == "FireServer" and (Args[1] == "Parry") and godmode then
+       return
+   end
+   if not checkcaller() and method == "FireServer" and (Args[1] == "Swing") and autoattack then
+       return
+   end
+   return oldnamecall(self, unpack(Args))
+end)
+setreadonly(mt, true)
+
+
+--// parry (god mode)
+local function parryAttacks(WeaponR, katana)
+    if godmode then
+        local args = {
+            [1] = "Parry",
+            [2] = katana,
+            [3] = true
+        }
+        WeaponR:FireServer(unpack(args))
+    end
+end
+
+--// Variables
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local RS = game:GetService("RunService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local ToggleR = ReplicatedStorage.ToggleRun
+local remoteName = ReplicatedStorage:WaitForChild("General"):WaitForChild("Config"):WaitForChild("wrn"):FindFirstChildOfClass("StringValue")
+
+if remoteName and remoteName.Value ~= "NOT_LOADED_YET" then 
+    local WeaponR = ReplicatedStorage:WaitForChild(remoteName.Value)
+
+    --// Always be parrying
+    do
+        local LPChar = LocalPlayer.Character
+        if LPChar then
+            local katana = LPChar:FindFirstChildOfClass("Tool")
+            if katana and katana:FindFirstChild("Weapon") then
+                parryAttacks(WeaponR, katana)
+            end
+            LPChar.ChildAdded:Connect(function(katana)
+                if katana:IsA("Tool") then
+                    katana:WaitForChild("Weapon")
+                    parryAttacks(WeaponR, katana)
+                end
+            end)
+        end
+        LocalPlayer.CharacterAdded:Connect(function(char)
+            char.ChildAdded:Connect(function(katana)
+                if katana:IsA("Tool") then
+                    katana:WaitForChild("Weapon")
+                    parryAttacks(WeaponR, katana)
+                end
+            end)
+        end)
+    end
+
+    _G.autoattack = true
+    while _G.autoattack do
+        local LPChar = LocalPlayer.Character
+        if LPChar then
+            local katana = LPChar:FindFirstChildOfClass("Tool")
+            if katana and katana:FindFirstChild("Weapon") then
+                --// the meat and bones of the script; autoattack
+                if autoattack then
+                    local args = {
+                        [1] = "Swing",
+                        [2] = katana
+                    }
+                    
+                    WeaponR:FireServer(unpack(args))
+                end
+
+                --// Run remote at sprint speed
+                if ws then
+                    local args = {
+                        [1] = walkspeedToChangeTo
+                    }
+
+                    ToggleR:FireServer(unpack(args))
+                end
+            end
+        end
+        wait(.1)
+    end
+end
