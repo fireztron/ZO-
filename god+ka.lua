d9a70a718f9592aaa284911de10ed5eecf4cd9e9
fireztron <84250829+fireztron@users.noreply.github.com>
2021-05-30 15:42:33 -0400

    Update god+ka.lua

--- god+ka.lua
+++ god+ka.lua
@@ -1,3 +1,39 @@
+--[[
+    Attacking manually after running this will not affect how fast you kill.
+    Idk what the difference between parrying and blocking but all i know is parrying is more op
+
+    Made by fireztron @ v3rm
+]]
+
+local mt = getrawmetatable(game) 
+local oldnamecall = mt.__namecall
+setreadonly(mt, false)
+
+--// Always parry
+mt.__namecall = newcclosure(function(self, ...)
+   local method = getnamecallmethod()
+   local Args = {...}
+   if not checkcaller() and method == "FireServer" and (Args[1] == "Parry" or Args[1] == "Swing") and _G.autoattack then
+       return
+   end
+   return oldnamecall(self, unpack(Args))
+end)
+setreadonly(mt, true)
+
+
+--// parry (god mode)
+local function parryAttacks(WeaponR, katana)
+    if _G.autoattack then
+        local args = {
+            [1] = "Parry",
+            [2] = katana,
+            [3] = true
+        }
+        WeaponR:FireServer(unpack(args))
+    end
+end
+
+--// Variables
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local RS = game:GetService("RunService")
@@ -8,21 +44,38 @@ local remoteName = ReplicatedStorage:WaitForChild("General"):WaitForChild("Confi
 
 if remoteName and remoteName.Value ~= "NOT_LOADED_YET" then 
     local WeaponR = ReplicatedStorage:WaitForChild(remoteName.Value)
+
+    --// Always be parrying
+    do
+        local LPChar = LocalPlayer.Character
+        if LPChar then
+            local katana = LPChar:FindFirstChildOfClass("Tool")
+            if katana and katana:FindFirstChild("Weapon") then
+                parryAttacks(WeaponR, katana)
+            end
+            LPChar.ChildAdded:Connect(function(katana)
+                if katana:IsA("Tool") then
+                    katana:WaitForChild("Weapon")
+                    parryAttacks(WeaponR, katana)
+                end
+            end)
+        end
+        LocalPlayer.CharacterAdded:Connect(function(char)
+            char.ChildAdded:Connect(function(katana)
+                if katana:IsA("Tool") then
+                    katana:WaitForChild("Weapon")
+                    parryAttacks(WeaponR, katana)
+                end
+            end)
+        end)
+    end
+
     _G.autoattack = true
     while _G.autoattack do
         local LPChar = LocalPlayer.Character
         if LPChar then
             local katana = LPChar:FindFirstChildOfClass("Tool")
             if katana and katana:FindFirstChild("Weapon") then
-                --// autoblock (god mode)
-                local args = {
-                    [1] = "Block",
-                    [2] = katana,
-                    [3] = true
-                }
-
-                WeaponR:FireServer(unpack(args))
-                
                 --// the meat and bones of the script; autoattack
                 local args = {
                     [1] = "Swing",
